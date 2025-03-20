1d1eaa6f4a3f9cb3a0915759357cd8a3acc52616
Nick Savage <nick@nicksavage.ca>
2025-03-20 18:47:21 -0400

    remove debugging

--- src/main.go
+++ src/main.go
@@ -305,7 +305,7 @@ func RunProgram(cpu *CPU, maxCycles int) {
 	start := time.Now()
 
 	for i := 0; i < maxCycles; i++ {
-		cycleStart := time.Now()
+		// cycleStart := time.Now()
 
 		cpu.HandleKeyboard()
 		cpu.HandleInterrupts()
@@ -333,11 +333,11 @@ func RunProgram(cpu *CPU, maxCycles int) {
 		// if cpu.Memory[0xFF44] == cpu.Memory[0xFF45] {
 		// 	cpu.RequestStatInterrupt()
 		// }
-		elapsed := time.Since(start)
-		cycleTime := time.Since(cycleStart)
-		avgCycleTime := elapsed / time.Duration(i+1)
-		log.Printf("Executed %d instructions, PC: 0x%04X, clock: %d, Avg cycle: %v, Current cycle: %v",
-			i, cpu.PC, cpu.Clock, avgCycleTime, cycleTime)
+		// elapsed := time.Since(start)
+		// cycleTime := time.Since(cycleStart)
+		// avgCycleTime := elapsed / time.Duration(i+1)
+		// log.Printf("Executed %d instructions, PC: 0x%04X, clock: %d, Avg cycle: %v, Current cycle: %v",
+		// 	i, cpu.PC, cpu.Clock, avgCycleTime, cycleTime)
 		err := cpu.CheckError()
 		if err != nil {
 			log.Printf("Test has failed")
